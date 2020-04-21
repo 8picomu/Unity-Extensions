8f97ac254c2a56e03f9019ea21bc0e1db8078c21
たた <pf35301@gmail.com>
2020-04-22 04:57:42 +0900

    [add] PrefabField

---
+++ Editor/PrefabFieldAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+namespace pf35301.Extensions.Editor {
+    public class PrefabFieldAttribute : PropertyAttribute {
+
+    }
+}
---
+++ Editor/PrefabFieldDrawer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace pf35301.Extensions.Editor {
+#if UNITY_EDITOR
+
+    [CustomPropertyDrawer(typeof(PrefabFieldAttribute))]
+    public class PrefabFieldDrawer : PropertyDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            if(property.objectReferenceValue != null) {
+                var prefabAssetType = PrefabUtility.GetPrefabAssetType(property.objectReferenceValue);
+                switch(prefabAssetType) {
+                    case PrefabAssetType.NotAPrefab:
+                        property.objectReferenceValue = null;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            label.text += " (Prefab Only)";
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+
+#endif
+}
