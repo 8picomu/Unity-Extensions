2259dd805fafaa02cd1d705258589326dc2b4f14
たた <pf35301@gmail.com>
2020-12-25 13:04:21 +0900

    [add] ListWrapper

---
+++ ListWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pf35301.Extensions {
+    public interface IListWrapper<T> {
+        List<T> List { get; set; }
+    }
+
+    /// <summary>
+    /// 多次元配列のシリアライズのときに使用する
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [Serializable]
+    public class ListWrapper<T> {
+        public List<T> List;
+    }
+}
