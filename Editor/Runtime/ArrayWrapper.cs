b7c6a7f66939225289c1fad9e207668957bc4744
たた <8picomu@gmail.com>
2021-03-01 14:01:49 +0900

    [change] ListWrapper -> ArrayWrapper

---
+++ Editor/Runtime/ArrayWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace eightpicomu.Extensions {
+    public interface IArrayWrapper<T> {
+        T[] Array { get; set; }
+    }
+
+    /// <summary>
+    /// 多次元配列のシリアライズのときに使用する
+    /// </summary>
+    [Serializable]
+    public class BoolArrayWrapper {
+        public bool[] Array;
+    }
+}
